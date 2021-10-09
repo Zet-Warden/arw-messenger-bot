8743e1b746bcdb8c99857a1a9abfa6be9b5f2d93
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-10-09 16:28:35 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 const { router, text, payload } = require('bottender/router');
 const quickReplies = require('./concerns/quickReplies');
+const concerns = require('./concerns/payload');
 
 module.exports = async function App(context) {
     return router([
         payload('GET_STARTED', quickReplies),
-        payload([
-            'arw_events',
-            'arw_concerns',
-            'payment_concerns',
-            'website_concerns',
-            'others',
-        ]),
+        payload(
+            [
+                'arw_events',
+                'arw_concerns',
+                'payment_concerns',
+                'website_concerns',
+                'others',
+            ],
+            concerns
+        ),
     ]);
 };
