2b5ecf1a65c648703bfd8b5077c71dde45f25221
Jared <jaredblase@gmail.com>
2021-10-09 17:04:34 +0800

    update: changed switch cases to match payload

--- src/arw-concerns.js
+++ src/arw-concerns.js
@@ -1,15 +1,15 @@
-const msg = context.event.payload;;
+const msg = context.event.payload;
 
 switch (msg) {
-  case "WHAT IS ANNUAL RECRUITMENT WEEK?":
+  case "about_arw":
     context.sendText("The Annual Recruitment Week is a week-long event wherein all 50 CSO accredited organizations are given the opportunity to attract and recruit students from the Lasallian community. The Annual Recruitment Week serves as the platform for these organizations to showcase who they are and what they can provide to their members through their scheduled activities for the academic year.");
     break;
 
-  case "WHERE DO I REGISTER TO APPLY FOR AN ORGANIZATION?":
+  case "register_organization":
     context.sendText("For registration, you can check out the ARW Website: arw.dlsucso.com");
     break;
   
-  case "UNTIL WHEN CAN I JOIN AN ORGANIZATION?":
+  case "join_organization":
     context.sendText("The Annual Recruitment Week 2021 will happen on November 22-27, 2021.  The last day of recruitment will be on November 27, 2021 at 9:00 PM.");
     break;
 }
--- src/website-concerns.js
+++ src/website-concerns.js
@@ -1,10 +1,10 @@
 const msg = context.event.payload;
 
 switch (msg) {
-  case "WHEN WILL THE WEBSITE OPEN?":
+  case "website_open":
     context.sendText("The ARW Website is accessible on November 21, 2021 at _:00 AM.");
     break;
-  case "IS THE WEBSITE FUNCTIONAL 24/7?":
+  case "website_24/7":
     context.sendText("Yes. The website can be accessed 24/7 but you can only join orgs from from 8:00 AM - 5:00 PM during the Annual Recruitment Week.");
     break;
 }
