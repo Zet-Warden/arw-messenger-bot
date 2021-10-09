ff2f974e6e9d327cf7ce40403631139deddf4a10
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-10-10 06:09:39 +0800

    refactored + added loop

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ const { chain } = require('bottender');
 const handlers = require('./HandlerList.js');
 
 module.exports = async function App(context) {
-<<<<<<< Updated upstream
-    return chain([init, ...handlers]);
-=======
     return chain([...handlers]);
->>>>>>> Stashed changes
 };
