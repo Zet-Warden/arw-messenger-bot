d17b426b8f0a53b4f14c15cadee9f83c06b83fa6
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-10-09 18:53:00 +0800

    Update others-concerns.js

--- src/others-concerns.js
+++ src/others-concerns.js
@@ -2,12 +2,12 @@ module.exports = async function (context) {
     const msg = context.event.payload;
 
     switch (msg) {
-        case 'join_org':
+        case 'ot_join_org':
             context.sendText(
                 'No, it is not required to join organizations. However, joining an organization allows you to meet various people and gain new experiences. It can also provide you with many opportunities that can help you grow and make your college life memorable.'
             );
             break;
-        case 'join_requirements':
+        case 'ot_join_requirements':
             context.sendText(
                 "It may vary depending on the organization's decisions."
             );
