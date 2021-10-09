2324c325b83c664df1e4b78d5859e9c78d650a6e
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-10-09 15:58:07 +0800

    added routes for quickreplies

---
+++ src/concerns/payload.js
@@ -0,0 +1,10 @@
+module.exports = async function Payload(context) {
+    switch(context.event.payload) {
+        case "arw events": break;
+        case "arw concerns": break;
+        case "payment concerns": break;
+        case "website concerns": break;
+        default: break;
+    }
+};
+  
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-const { router, text } = require('bottender/router');
-const quickReplies = require('./quickreplies')
+const { router, text, payload } = require('bottender/router');
+const quickReplies = require('./concerns/quickreplies')
 
 module.exports = async function App(context) {
   return router([
     text('hi', quickReplies),
+    payload(['arw events', 'arw concerns', 'payment concerns', 'website concerns', 'others'])
   ]);
 };
