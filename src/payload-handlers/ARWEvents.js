c8ca96b574862be998fa30f6aa88f840f1f6d333
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-10-09 22:34:31 +0800

    Create ARWEvents.js

---
+++ src/payload-handlers/ARWEvents.js
@@ -0,0 +1,29 @@
+const Handler = require('./Handler.js');
+
+const { museum, elite } = require('../init/buttonTemplates.js');
+const ARWEventHandler = new Handler('ae');
+
+ARWEventHandler.addEvents([
+    {
+        name: 'arw_events',
+        action: (context) => {
+            context.sendText(
+                "This year's ARW will have 2 main events: It's Museum Day, Everyday and the ARW Elite."
+            );
+        },
+    },
+    {
+        name: 'arw_museum',
+        action: (context) => {
+            museum(context);
+        },
+    },
+    {
+        name: 'arw_elite',
+        action: (context) => {
+            elite(context);
+        },
+    },
+]);
+
+module.exports = ARWEventHandler;
