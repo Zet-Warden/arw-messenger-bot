dc794d784a46dc3c006ff4c16f46d53c70505484
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-10-18 18:32:28 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ const { chain } = require('bottender');
 const handlers = require('./HandlerList.js');
 
 module.exports = async function App(context) {
+    console.log('init');
     return chain([init, ...handlers]);
 };
