cdd900fcc3d48c3e8738df11b8a4ac4758850349
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-10-18 17:39:22 +0800

    updated bot

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ const { chain } = require('bottender');
 const handlers = require('./HandlerList.js');
 
 module.exports = async function App(context) {
-    // return chain([init, ...handlers]);
-
-    await context.sendText('hi', { personaId: '337754377725622' });
-    await context.sendText('hi', { personaId: '1542383836115368' });
+    return chain([init, ...handlers]);
 };
